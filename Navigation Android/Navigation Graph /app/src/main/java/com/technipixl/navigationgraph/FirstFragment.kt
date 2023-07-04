41c9cfdc2dae2a2b73cd7ce2bd89d12b863c0e7c
Valérian Paillet <paillet.valerian@gmail.com>
2023-07-04 15:41:41 +0200

    add navigationgraph exo

---
+++ Navigation Android/Navigation Graph /app/src/main/java/com/technipixl/navigationgraph/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.technipixl.navigationgraph
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.technipixl.navigationgraph.databinding.ActivityMainBinding
+import com.technipixl.navigationgraph.databinding.FragmentFirstBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class FirstFragment : Fragment() {
+    lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.goSecondFragment.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("message", "Hello Second Fragment")
+            val action = R.id.action_firstFragment_to_secondFragment
+            findNavController().navigate(action,bundle)
+        }
+
+    }
+}
