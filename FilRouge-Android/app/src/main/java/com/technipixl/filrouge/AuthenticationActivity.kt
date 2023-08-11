931ec879bd622e1debb7dfa9a29465eac96b913e
Valérian Paillet <paillet.valerian@gmail.com>
2023-08-11 13:46:55 +0200

    update fill rouge login page

--- FilRouge-Android/app/src/main/java/com/technipixl/filrouge/AuthenticationActivity.kt
+++ FilRouge-Android/app/src/main/java/com/technipixl/filrouge/AuthenticationActivity.kt
@@ -1,73 +1,79 @@
 package com.technipixl.filrouge
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.core.view.contains
-import androidx.core.view.isEmpty
-import androidx.core.view.isNotEmpty
+import android.widget.EditText
 import androidx.core.widget.doAfterTextChanged
-import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
 import com.technipixl.filrouge.databinding.ActivityAuthentificationBinding
 import java.util.regex.Pattern
 
 class AuthenticationActivity : AppCompatActivity() {
     lateinit var binding: ActivityAuthentificationBinding
 
-    private var emailValid: Boolean = false
-    private var passwordValid: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthentificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        //binding.emailInputLayout.isErrorEnabled
-        //binding.emailInputLayout.error = "Error"
+        binding.loginButton.background = getDrawable(R.drawable.disabled_button)
+        binding.loginButton.isEnabled = false
     }
 
     override fun onStart() {
         super.onStart()
 
-        binding.emailEditText.doAfterTextChanged {
-            if(binding.emailEditText.text.toString().matches(android.util.Patterns.EMAIL_ADDRESS.toRegex())) {
-                binding.emailInputLayout.isErrorEnabled = false
-                emailValid = true
-                binding.emailEditText.setTextColor(getColor(R.color.green))
-            } else {
-                emailValid = false
-                binding.emailInputLayout.isErrorEnabled = true
-                binding.emailInputLayout.error = "L'email n'est pas valide"
-                binding.emailEditText.setTextColor(getColor(R.color.red))
-            }
-        }
+        updateTextField(binding.emailInputLayout, binding.emailEditText, "L'email n'est pas valide")
+        updateTextField(
+            binding.passwordInputLayout,
+            binding.passwordEditText,
+            "Le mot de passe n'est pas valide"
+        )
 
-        binding.passwordEditText.doAfterTextChanged {
-            if(binding.passwordEditText.text.toString().matches(Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$").toRegex())) {
-                binding.passwordInputLayout.isErrorEnabled = false
-                passwordValid = true
-                binding.passwordEditText.setTextColor(getColor(R.color.green))
-            } else {
-                passwordValid = false
-                binding.passwordInputLayout.isErrorEnabled = true
-                binding.passwordInputLayout.error = "Le mot de passe n'est pas valide"
-                binding.passwordEditText.setTextColor(getColor(R.color.red))
-            }
-        }
     }
 
     fun openHome(view: View) {
-        val emailNotEmpty = binding.emailEditText.text.toString().isNotEmpty()
-        val passwordNotEmpty = binding.passwordEditText.text.toString().isNotEmpty()
-
-        if(passwordValid && emailValid){
+        if (isValid(binding.emailEditText) && isValid(binding.passwordEditText)) {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun isValid(editText: EditText): Boolean {
+        return if (editText == binding.emailEditText) {
+            editText.text.toString().matches(android.util.Patterns.EMAIL_ADDRESS.toRegex())
+        } else {
+            editText.text.toString().matches(
+                Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+                    .toRegex()
+            )
+        }
+    }
+
+    fun updateTextField(textLayout: TextInputLayout, editText: EditText, errorMessage: String) {
+        editText.doAfterTextChanged {
+            if (isValid(editText)) {
+                textLayout.isErrorEnabled = false
+                editText.setTextColor(getColor(R.color.green))
+            } else {
+                textLayout.isErrorEnabled = true
+                textLayout.error = errorMessage
+                editText.setTextColor(getColor(R.color.red))
+            }
+
+            updateButton()
+        }
+    }
+
+    fun updateButton() {
+        if (isValid(binding.emailEditText) && isValid(binding.passwordEditText)) {
+            binding.loginButton.background = getDrawable(R.drawable.input_rounded_green_button)
+            binding.loginButton.isEnabled = true
+        } else {
+            binding.loginButton.background = getDrawable(R.drawable.disabled_button)
+            binding.loginButton.isEnabled = false
         }
     }
 }
